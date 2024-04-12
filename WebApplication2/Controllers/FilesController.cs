be45459fa639d467239c204da1446fe7655f260d
aakhmatov <aakhmatov@UZB815-12-0722>
2024-04-12 21:02:24 +0500

    Add project files.

---
+++ WebApplication2/Controllers/FilesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FilesController : ControllerBase
+{
+    [HttpGet]
+    public async ValueTask<IActionResult> GetAll()
+    {
+        return Ok("sfs");
+    }
+    [HttpPost]
+    public async ValueTask<IActionResult> Create()
+    {
+        return Ok("sfs");
+    }
+    [HttpDelete]
+    public async ValueTask<IActionResult> Delete()
+    {
+        return Ok("sfs");
+    }
+    [HttpPut]
+    public async ValueTask<IActionResult> Update()
+    {
+        return Ok("sfs");
+    }
+    [HttpHead]
+    public async ValueTask<IActionResult> Head()
+    {
+        return Ok("sfs");
+    }
+    [HttpOptions]
+    public async ValueTask<IActionResult> Options()
+    {
+        return Ok("sfs");
+    }
+}
---
+++ WebApplication2/Controllers/ValuesController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace WebApplication2.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ValuesController : ControllerBase
+{
+    private Car[] Cars { get; set; }
+    public ValuesController()
+    {
+        Cars = new Car[]
+        {
+            new Car()
+            {
+                Model = "Supra",
+                Color = "Red",
+                Type = CarType.Sport
+            },
+            new Car()
+            {
+                Model = "Nissan GTR-35",
+                Color = "White",
+                Type = CarType.Sport
+            },
+            new Car()
+            {
+                Model = "Mercedes",
+                Color = "Black",
+                Type = CarType.Business
+            },
+            new Car()
+            {
+                Model = "Porsche",
+                Color = "Yellow",
+                Type = CarType.Sport
+            }
+        };
+    }
+    /// <summary>
+    /// what's upo homie
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("/cars")]
+
+    [SwaggerOperation(Summary = "You can get delist of cars", Description = "")]
+    public IActionResult GetList()
+    {
+        Debug.WriteLine(Cars.Length);
+        return Ok(Cars);
+    }
+}
+internal class Car
+{
+    public string Model { get; set; }
+    public string Color { get; set; }
+    public CarType Type { get; set; }
+}
+
+public enum CarType
+{
+    Sport = 1,
+    Business = 2
+}
